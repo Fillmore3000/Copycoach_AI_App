0271a99ef63b8482cfa5a17698c3a53b116c6cb5
Henry Apaw <45966616+Fillmore3000@users.noreply.github.com>
2022-07-20 05:05:19 +0100

    Add files via upload

--- copykitt-site/next.config.js
+++ copykitt-site/next.config.js
@@ -1,30 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' }np
-    }
-  },  
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-
-  rules: [{
-    test: /\.(gif|png|jpe?g|svg)$/i,
-    use: [
-      'file-loader',
-      {
-        loader: 'akamai',
-        options: {
-          bypassOnDebug: true, // webpack@1.x
-          disable: true, // webpack@2.x and newer
-        },
-      },
-    ],
-  }]
-
+  reactStrictMode: true,
 }
